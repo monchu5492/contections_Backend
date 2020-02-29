1a922b2a3b55b7ce6a0304f838be250c7016d41d
Tanner Patterson <tpatterson5492@gmail.com>
2020-02-29 15:20:16 -0800

    sean helped set up serializers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :join_events, dependent: :destroy
-    has_many :events,
-    :through => :join_events, dependent: :destroy
+    has_many :events, dependent: :destroy
 end
     
